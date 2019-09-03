8b5fd824cd8ac5477b4acbe84470654dd6778211
zhangfaming <1005603072@qq.com>
2019-09-03 19:53:24 +0800

    项目初始化

---
+++ src/main/java/tk/mybatis/springboot/Application.java
@@ -0,0 +1,37 @@
+package tk.mybatis.springboot;
+
+//特别注意，下面的是 tk.MapperScan
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import tk.mybatis.spring.annotation.MapperScan;
+
+
+@Controller
+@EnableWebMvc
+@SpringBootApplication
+@MapperScan(basePackages = "tk.mybatis.springboot.mapper")
+public class Application extends WebMvcConfigurerAdapter implements CommandLineRunner {
+    private Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("服务启动完成!");
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "redirect:countries";
+    }
+}
